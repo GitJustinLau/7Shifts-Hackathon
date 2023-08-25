89fd161de64b007160346e8ae2e0336d458655e9
Justin Lau <justinlau094@hotmail.com>
2023-08-25 11:08:00 -0400

    setup ob1

---
+++ src/pages/OnboardingPage1/OnboardingPage1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '@7shifts/sous-chef/dist/index.css';
+import sideBar from '../../assets/images/side-bar.png';
+
+const OnboardingPage1 = () => {
+    return (
+        <main className='on-board-1'>
+
+        </main>
+    );
+};
+
+export default OnboardingPage1;
