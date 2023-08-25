264dd0c35af1051f39cc6e12ed87f4669ef4845c
Justin Lau <justinlau094@hotmail.com>
2023-08-25 14:25:45 -0400

    homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss"
-import HomePage from "./pages/HomePage/HomePage";
-import OnboardingPage from "./pages/OnboardingPage/OnboardingPage";
 import '@7shifts/sous-chef/dist/index.css';
 import OnboardingPage2 from "./pages/OnboardingPage2/OnboardingPage2";
 import OnboardingPage1 from "./pages/OnboardingPage1/OnboardingPage1";
@@ -10,8 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<OnboardingPage />} />
-        <Route path="/onboarding1" element={<OnboardingPage1 />} />
+        <Route path="/" element={<OnboardingPage1 />} />
         <Route path="/onboarding2" element={<OnboardingPage2 />} />
       </Routes>
     </BrowserRouter>
