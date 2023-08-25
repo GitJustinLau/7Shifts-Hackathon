52f3e8c4543becb4ed360aa188ec1f41d3978f9c
Sky-Roh <haneul.sky.roh@gmail.com>
2023-08-25 10:49:58 -0400

    basic set up

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import '@7shifts/sous-chef/dist/index.css';
+import {
+    Form,
+    FormRow,
+    TextField,
+    FormFooter, Button} from '@7shifts/sous-chef'
+
 
 const HomePage = () => {
     return (
         <div>
             <h1>welcome</h1>
-        </div>
+            <Form>
+                <FormRow>
+                    <TextField name="firstName" label="First Name"/>
+                    <TextField name="lastName" label="Last Name"/>
+                </FormRow>
+                <TextField name="address" label="Address"/>
+                <FormFooter actions={
+                    {
+                        primary: <Button onClick={
+                            () => {}
+                        }>Save</Button>
+                    }
+                }/>
+            </Form>
+    </div>
     );
 };
 
