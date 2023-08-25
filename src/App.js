65895a5b6f06d8dd85bc748f93bf2517cf9a54b0
Justin Lau <justinlau094@hotmail.com>
2023-08-25 14:06:31 -0400

    change homepage url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss"
-import HomePage from "./pages/HomePage/HomePage";
 import OnboardingPage from "./pages/OnboardingPage/OnboardingPage";
 import '@7shifts/sous-chef/dist/index.css';
 import OnboardingPage1 from "./pages/OnboardingPage1/OnboardingPage1";
@@ -9,9 +8,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
-        <Route path="/onboarding1" element={<OnboardingPage1 />} />
+        <Route path="/" element={<OnboardingPage1 />} />
       </Routes>
     </BrowserRouter>
   );
