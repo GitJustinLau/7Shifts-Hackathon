4df99f302870f8f126cb1420fda917f22a29ef96
Sky-Roh <haneul.sky.roh@gmail.com>
2023-08-25 13:04:57 -0400

    finished without functionality

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import Onboarding3 from '../../components/Onboarding3/Onboarding3';
-
 
 
 const HomePage = () => {
     return (
         <div>
-            <Onboarding3 />
-    </div>
+            <h1>homepage</h1>
+        </div>
     );
 };
 
--- src/pages/OnboardingPage/OnboardingPage.js
+++ src/pages/OnboardingPage/OnboardingPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Onboarding3 from '../../components/Onboarding3/Onboarding3';
 
 const OnboardingPage = () => {
     return (
         <div>
-            <h1>onboarding page</h1>
+            < Onboarding3 />
         </div>
     );
 };
